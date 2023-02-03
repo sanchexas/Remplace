98ede94eca181f702d7f57214abc6421c60e488f
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-02-03 15:56:31 +0300

    added .env for u boyz

--- server/dist/index.js
+++ server/dist/index.js
@@ -3,12 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require('dotenv').config();
 const express_1 = __importDefault(require("express"));
-const PORT = 3005;
+const PORT = process.env.PORT;
 const app = (0, express_1.default)();
+const cors = require('cors');
 app.listen(PORT, () => {
     console.log(`Server works on PORT ${PORT}`);
 });
 app.get('/', (req, res) => {
     res.status(200).json("Server works");
 });
+//testing commits after renaming repo
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
+require('dotenv').config();
 import express from 'express';
-
-const PORT: number = 3005;
+const PORT = process.env.PORT;
 const app = express();
+const cors = require('cors');
+
 
 app.listen(PORT, ()=>{
     console.log(`Server works on PORT ${PORT}`);
