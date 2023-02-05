3fbaaaca75e57da939a78d597d8f982fff25b99d
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-02-05 09:07:44 +0300

    found an issue :(

--- server/dist/Services/user.service.js
+++ server/dist/Services/user.service.js
@@ -1,11 +1,20 @@
+/**
+ * (EN) The interface fields are not checked because we import .js (as I understood), which does't support typescript interfaces.
+ * If I type "result[0].blablabla" the compiler will not find an error. But in fact it should find an error.
+ * In any case I specified the type of the variable "result" (***) because it is according to the rules of typescript.
+ *
+ * (RU) Поля интерфейса не проверяются, потому что используется импорт .js (как я понял), который не поддерживает интерфейсы typescript.
+ * Если я напечатаю "result[0].blablabla", компилятор не обнаружит ошибку. Но на самом деле он должен.
+ * В любом случае, я указал тип переменной "result", потому что это соответствует правилам typescript.
+ */
 import { UserRepository } from "../Repositories/user.repository.js";
 export class UserService {
     async getAll() {
-        const urr = new UserRepository;
+        const ur = new UserRepository;
         try {
             let testVariable;
-            await urr.getAll().then((result) => {
-                testVariable = result;
+            await ur.getAll().then((result) => {
+                testVariable = result[0].login; // ***
             });
             return testVariable;
         }
--- server/src/Services/user.service.ts
+++ server/src/Services/user.service.ts
@@ -1,13 +1,24 @@
+/**
+ * (EN) The interface fields are not checked because we import .js (as I understood), which does't support typescript interfaces. 
+ * If I type "result[0].blablabla" the compiler will not find an error. But in fact it should find an error. 
+ * In any case I specified the type of the variable "result" (***) because it is according to the rules of typescript.
+ * 
+ * (RU) Поля интерфейса не проверяются, потому что используется импорт .js (как я понял), который не поддерживает интерфейсы typescript.
+ * Если я напечатаю "result[0].blablabla", компилятор не обнаружит ошибку. Но на самом деле он должен.
+ * В любом случае, я указал тип переменной "result", потому что это соответствует правилам typescript.
+ */
+
+import { IUserModel } from "../Models/user.model.js";
 import { UserRepository } from "../Repositories/user.repository.js";
 
 export class UserService{
     async getAll(){
-        const urr = new UserRepository;
+        const ur = new UserRepository;
         try{
             let testVariable;
-            await urr.getAll().then((result: Object)=>{
-                testVariable = result;
-            })
+            await ur.getAll().then((result: IUserModel[])=>{ // ***
+                testVariable = result[0].login; // ***
+            });
             return testVariable;
         }catch(e){
             return "Errorrrrrrrr";
