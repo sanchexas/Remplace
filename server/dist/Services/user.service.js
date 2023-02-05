875e0990eef30e232b5a6e967dd19a69ea5da47b
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-02-05 09:25:39 +0300

    found an issue2

--- server/dist/Services/user.service.js
+++ server/dist/Services/user.service.js
@@ -1,7 +1,9 @@
 /**
+ * ISSUE
+ *
  * (EN) The interface fields are not checked because we import .js (as I understood), which does't support typescript interfaces.
  * If I type "result[0].blablabla" the compiler will not find an error. But in fact it should find an error.
- * In any case I specified the type of the variable "result" (***) because it is according to the rules of typescript.
+ * In any case I specified the type of the variable "result" (***ISSUE) because it is according to the rules of typescript.
  *
  * (RU) Поля интерфейса не проверяются, потому что используется импорт .js (как я понял), который не поддерживает интерфейсы typescript.
  * Если я напечатаю "result[0].blablabla", компилятор не обнаружит ошибку. Но на самом деле он должен.
@@ -14,7 +16,7 @@ export class UserService {
         try {
             let testVariable;
             await ur.getAll().then((result) => {
-                testVariable = result[0].login; // ***
+                testVariable = result[0].login; // ***ISSUE
             });
             return testVariable;
         }
--- server/src/Services/user.service.ts
+++ server/src/Services/user.service.ts
@@ -1,7 +1,9 @@
 /**
+ * ISSUE
+ * 
  * (EN) The interface fields are not checked because we import .js (as I understood), which does't support typescript interfaces. 
  * If I type "result[0].blablabla" the compiler will not find an error. But in fact it should find an error. 
- * In any case I specified the type of the variable "result" (***) because it is according to the rules of typescript.
+ * In any case I specified the type of the variable "result" (***ISSUE) because it is according to the rules of typescript.
  * 
  * (RU) Поля интерфейса не проверяются, потому что используется импорт .js (как я понял), который не поддерживает интерфейсы typescript.
  * Если я напечатаю "result[0].blablabla", компилятор не обнаружит ошибку. Но на самом деле он должен.
@@ -16,8 +18,8 @@ export class UserService{
         const ur = new UserRepository;
         try{
             let testVariable;
-            await ur.getAll().then((result: IUserModel[])=>{ // ***
-                testVariable = result[0].login; // ***
+            await ur.getAll().then((result: IUserModel[])=>{ // ***ISSUE
+                testVariable = result[0].login; // ***ISSUE
             });
             return testVariable;
         }catch(e){
