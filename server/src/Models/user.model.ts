e1d40a9e0e1ce9fe67ff2ece6d36182b1669abf3
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-02-07 21:44:54 +0300

    simple comm

--- server/src/Models/user.model.ts
+++ server/src/Models/user.model.ts
@@ -1,6 +1,6 @@
 import { RowDataPacket } from "mysql2";
 // Test model
-export interface IUserModel extends RowDataPacket{ //need to learn better about rowdatapacket
+export interface IUserModel extends RowDataPacket{ 
     id?: number
     login: string
 }
