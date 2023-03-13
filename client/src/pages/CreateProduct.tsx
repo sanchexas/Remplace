2e85cb5ee74534dbfe32d6d63e8de6b7eae545a7
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-03-13 12:43:00 +0300

    changed preview

--- client/src/pages/CreateProduct.tsx
+++ client/src/pages/CreateProduct.tsx
@@ -101,23 +101,24 @@ const CreateProduct = () =>{
                 <div className='product__preview'>
                     <span className='IM' style={{fontSize: "20px"}}>Так пользователи будут видеть ваш продукт:</span>
                     <div className='product__card'>
-                        <div className='product__img'>
-                            <img src={(preview !== undefined) ? preview : defaultImage} alt='product pic' />
-                        </div>
-                        <div className='product__card__info'>
-                            <span className='FS_20 IR'>{title}</span>
-                            <span className='FS_20 IBl'>{price} ₽</span>
-                            <div className='product__card__actions'>
-                                <button className='add__to__cart'>В корзину</button>
-                                <button>
-                                    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M8.75 3.75C5.29875 3.75 2.5 6.52 2.5 9.9375C2.5 12.6962 3.59375 19.2438 14.36 25.8625C14.5529 25.9798 14.7743 26.0419 15 26.0419C15.2257 26.0419 15.4471 25.9798 15.64 25.8625C26.4062 19.2438 27.5 12.6962 27.5 9.9375C27.5 6.52 24.7012 3.75 21.25 3.75C17.7988 3.75 15 7.5 15 7.5C15 7.5 12.2012 3.75 8.75 3.75Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                                    </svg>
-                                </button>
+                        <div>
+                            <div className='product__img'>
+                                <img src={(preview !== undefined) ? preview : defaultImage} alt='product pic' />
+                            </div>
+                            <div className='product__card__info'>
+                                <span className='FS_20 IR' style={{position: "absolute", top: "20px"}}>{title}</span>
+                                <span className='FS_20 IBl' style={{position: "absolute", top: "80px"}}>{price} ₽</span>
                             </div>
                         </div>
+                        <div className='product__card__actions'>
+                            <button className='add__to__cart'>В корзину</button>
+                            <button>
+                                <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M8.75 3.75C5.29875 3.75 2.5 6.52 2.5 9.9375C2.5 12.6962 3.59375 19.2438 14.36 25.8625C14.5529 25.9798 14.7743 26.0419 15 26.0419C15.2257 26.0419 15.4471 25.9798 15.64 25.8625C26.4062 19.2438 27.5 12.6962 27.5 9.9375C27.5 6.52 24.7012 3.75 21.25 3.75C17.7988 3.75 15 7.5 15 7.5C15 7.5 12.2012 3.75 8.75 3.75Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                </svg>
+                            </button>
+                        </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
