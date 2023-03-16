3d486c671043ba30bd808f85f59a0049f986055a
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-03-16 16:04:27 +0300

    fetch categories

--- client/src/pages/Catalogue.tsx
+++ client/src/pages/Catalogue.tsx
@@ -1,8 +1,28 @@
+import { useEffect, useState } from 'react';
 import '../style.css';
+import CategoryController from '../controllers/CategoryController';
+import { ICategoryResponse } from '../models/responses/ICategoryResponse';
+import { Link } from 'react-router-dom';
 
 const Catalogue = () => {
+    const [categories, setCategories] = useState();
+    useEffect(()=>{
+        CategoryController.getAll().then((response)=>{
+            const resArr = response.data.message;
+            setCategories(resArr.map((category: ICategoryResponse)=>{
+                return(
+                    <Link to='' className='category__item'>
+                        {category.title}
+                    </Link>
+                );
+            }))
+        });
+    }, []);
     return(
-        <div>Каталог</div>
+        <div style={{display: "flex", flexDirection: "column", gap: "25px"}}>
+            <h1>Каталог</h1>
+            {categories}
+        </div>
     );
 }
 
