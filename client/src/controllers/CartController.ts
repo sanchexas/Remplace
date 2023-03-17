a97a214f9008c7de7bacdd6d03eecf01f1d41494
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-03-17 21:43:15 +0300

    cart setup

---
+++ client/src/controllers/CartController.ts
@@ -0,0 +1,23 @@
+import Axios from 'axios';
+Axios.defaults.withCredentials = true;
+
+class CartController{
+    //Работа с localstorage. 
+    addToCart(){
+
+    }
+    deleteFromCart(){
+
+    }
+    addQuantity(){
+
+    }
+    subQuantity(){
+
+    }
+    saveProducts(){
+        
+    }
+}
+
+export default new CartController();
