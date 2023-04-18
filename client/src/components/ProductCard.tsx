8e70c12872a375585de8a5ceb9ccc5f38271165e
sergey-sanches-peres <sanchesgithub@mail.ru>
2023-04-18 16:06:21 +0300

    added state for cart button

--- client/src/components/ProductCard.tsx
+++ client/src/components/ProductCard.tsx
@@ -14,7 +14,7 @@ type productCardProps = {
 }
 
 const ProductCard = ({id, image, title, price, quantity}: productCardProps) => {
-    // const [updateIsInCart, setUpdateIsInCart] = useState<boolean>();
+    const [putInCart, setPutInCart] = useState<boolean>(false);
     function addToCartHandler(){
         const product: CartProductModel = {
             id: id,
@@ -25,10 +25,9 @@ const ProductCard = ({id, image, title, price, quantity}: productCardProps) => {
             cart_quantity: 1,
             quantity: quantity
         };
+        setPutInCart(true);
         CartController.addToCart(product);
-        // setUpdateIsInCart(true);
     }
-    // console.log(CartController.isInCart(id));
 
     return(
         <div className='product__card'>
@@ -42,7 +41,7 @@ const ProductCard = ({id, image, title, price, quantity}: productCardProps) => {
                 </div>
             </Link>
             <div className='product__card__actions'>
-                <button  className={CartController.isInCart(id) ? 'add__to__cart incart__button' : 'add__to__cart'} disabled={CartController.isInCart(id) ? true : false} onClick={()=>addToCartHandler()}>В корзину</button>
+                <button  className={CartController.isInCart(id) || putInCart ? 'add__to__cart incart__button' : 'add__to__cart'} disabled={CartController.isInCart(id) ? true : false} onClick={()=>addToCartHandler()}>В корзину</button>
                 <button>
                     <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8.75 3.75C5.29875 3.75 2.5 6.52 2.5 9.9375C2.5 12.6962 3.59375 19.2438 14.36 25.8625C14.5529 25.9798 14.7743 26.0419 15 26.0419C15.2257 26.0419 15.4471 25.9798 15.64 25.8625C26.4062 19.2438 27.5 12.6962 27.5 9.9375C27.5 6.52 24.7012 3.75 21.25 3.75C17.7988 3.75 15 7.5 15 7.5C15 7.5 12.2012 3.75 8.75 3.75Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
